82e543809cbca927c4011763e6665c388c7e5987
Marta Fernandez Garcia <mfg306@inlumine.ual.es>
2021-10-31 22:44:07 +0100

    Pruebas

---
+++ appLPS/Prueba.swift
@@ -0,0 +1,15 @@
+//
+//  Prueba.swift
+//  
+//
+//  Created by Marta Fernandez Garcia on 31/10/21.
+//
+
+import Foundation
+import FirebaseCore
+
+class Prueba {
+    
+    FirebaseApp.configure()
+    let db = Firestore.firestore()
+}
---
+++ appLPS/appLPS/Prueba.swift
@@ -0,0 +1,16 @@
+//
+//  Prueba.swift
+//  appLPS
+//
+//  Created by Marta Fernandez Garcia on 31/10/21.
+//
+
+import Foundation
+import Firebase
+
+class Prueba {
+    
+    func inicializar() {
+        FirebaseApp.configure()
+    }
+}
