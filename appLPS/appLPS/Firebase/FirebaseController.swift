8374e3fd7b72e1ca353d6543b44a945a74162b35
Marta Fernandez Garcia <mfg306@inlumine.ual.es>
2021-11-09 14:02:44 +0100

    Intentando hacer la subida de imagenes

--- appLPS/appLPS/Firebase/FirebaseController.swift
+++ appLPS/appLPS/Firebase/FirebaseController.swift
@@ -9,65 +9,158 @@ import Foundation
 import Firebase
 import FirebaseAuth
 import FirebaseDatabase
-
 import SwiftUI
 import Firebase
 import Combine
+import UIKit
+import FirebaseStorage
 
 class FirebaseController: ObservableObject {
     
-    var didChange = PassthroughSubject<FirebaseController, Never>()
-        var session: User? { didSet { self.didChange.send(self) }}
-        var handle: AuthStateDidChangeListenerHandle?
-
-        func listen () {
-            // monitor authentication changes using firebase
-            handle = Auth.auth().addStateDidChangeListener { (auth, user) in
-                if let user = user {
-                    // if we have a user, create a new user model
-                    print("Got user: \(user)")
-                    self.session = User(
-                        uid: user.uid,
-                        displayName: user.displayName,
-                        email: user.email
-                    )
-                } else {
-                    // if we don't have a user, set our session to nil
-                    self.session = nil
+    let storage = Storage.storage()
+    var imagenes: [Image] = []
+    
+    func getData(correoUsuario: String)  -> [Image] {
+        let db = Firestore.firestore()
+        
+        db.collection("Files").getDocuments() { (querySnapshot, err) in
+            if let err = err {
+                print("Error getting documents: \(err)")
+            } else {
+                for document in querySnapshot!.documents {
+                    print("\(document.documentID) => \(document.data())")
+                    
+                    /*if  (document.data()["name"] as! String) == correoUsuario {
+                     
+                     }*/
                 }
             }
         }
+        
+        
+        self.imagenes =  self.getAllStorage()
+        
+        print(" ------ AQUI")
+        print(self.imagenes)
+        print("AQUI ----- ")
+        
+        return self.imagenes
+    }
     
-    func signUp(
-            email: String,
-            password: String,
-            handler: @escaping AuthDataResultCallback
-            ) {
-            Auth.auth().createUser(withEmail: email, password: password, completion: handler)
-        }
-
-        func signIn(
-            email: String,
-            password: String,
-            handler: @escaping AuthDataResultCallback
-            ) {
-            Auth.auth().signIn(withEmail: email, password: password, completion: handler)
-        }
-
-        func signOut () -> Bool {
-            do {
-                try Auth.auth().signOut()
-                self.session = nil
-                return true
-            } catch {
-                return false
+    func getAllStorage() -> [Image] {
+        let storageRef = storage.reference().child("images")
+        
+        
+        
+        
+        /// async
+        storageRef.listAll { (result, error) in
+            if let error = error {
+                print("Error while listing all files: ", error)
+            }
+            
+            for item in result.items {
+                print("Item in images folder: ", item)
+                print("METIENDOLO EN LA LISTA ----- ")
+                self.imagenes.append(Image(item.fullPath))
+                print("EIRHEIRHEREIROER----- ")
+                print( self.imagenes)
+                
             }
         }
-         
-    func unbind () {
-            if let handle = handle {
-                Auth.auth().removeStateDidChangeListener(handle)
+    
+    
+    
+    print("DEVOLVIENDO EL RESULTADO DESDE GETALLSOTRAGE----- ")
+    print( self.imagenes)
+    
+    return self.imagenes
+}
+
+func uploadStorage(image: UIImage) {
+    // https://designcode.io/swiftui-advanced-handbook-firebase-storage
+    let storageRef = storage.reference().child("images/\(NSDate())")
+    
+    /*let resizedImage = image.aspectFittedToHeight(200)*/
+    let data = image.jpegData(compressionQuality: 0.2)
+    
+    let metadata = StorageMetadata()
+    metadata.contentType = "image/jpg"
+    
+    if let data = data {
+        storageRef.putData(data, metadata: metadata) { (metadata, error) in
+            if let error = error {
+                print("Error while uploading file: ", error)
+            }
+            
+            if let metadata = metadata {
+                print("Metadata: ", metadata)
             }
         }
-    
+    }
+}
+
+func deleteItemStorage(item: StorageReference) {
+    item.delete { error in
+        if let error = error {
+            print("Error deleting item", error)
+        }
+    }
+}
+
+
+var didChange = PassthroughSubject<FirebaseController, Never>()
+var session: User? { didSet { self.didChange.send(self) }}
+var handle: AuthStateDidChangeListenerHandle?
+
+func listen () {
+    // monitor authentication changes using firebase
+    handle = Auth.auth().addStateDidChangeListener { (auth, user) in
+        if let user = user {
+            // if we have a user, create a new user model
+            print("Got user: \(user)")
+            self.session = User(
+                uid: user.uid,
+                displayName: user.displayName,
+                email: user.email
+            )
+        } else {
+            // if we don't have a user, set our session to nil
+            self.session = nil
+        }
+    }
+}
+
+func signUp(
+    email: String,
+    password: String,
+    handler: @escaping AuthDataResultCallback
+) {
+    Auth.auth().createUser(withEmail: email, password: password, completion: handler)
+}
+
+func signIn(
+    email: String,
+    password: String,
+    handler: @escaping AuthDataResultCallback
+) {
+    Auth.auth().signIn(withEmail: email, password: password, completion: handler)
+}
+
+func signOut () -> Bool {
+    do {
+        try Auth.auth().signOut()
+        self.session = nil
+        return true
+    } catch {
+        return false
+    }
+}
+
+func unbind () {
+    if let handle = handle {
+        Auth.auth().removeStateDidChangeListener(handle)
+    }
+}
+
 }
--- appLPS/appLPS/MainView.swift
+++ appLPS/appLPS/MainView.swift
@@ -6,19 +6,63 @@
 //
 
 import SwiftUI
+import FirebaseStorage
 
 struct MainView: View {
     @EnvironmentObject var session: FirebaseController
-
-    var body: some View {     
+    @State var mostrarImagePicker: Bool = false
+    @State var imageGeneral: UIImage = UIImage()
+    @State var imagenes: [Image] = []
+    
+    
+    var body: some View {
         
-        Group {
-          if (session.session != nil) {
-            Text("Hello user!")
-          } else {
-            LoginView()
-          }
-        }.onAppear(perform: getUser)
+        VStack {
+            if (session.session != nil) {
+                
+                Button(){
+                    mostrarImagePicker.toggle() }label:{
+                        Image(uiImage: imageGeneral) .resizable()
+                            .frame(width: 90, height:90)
+                            .cornerRadius(100)
+                            .overlay(RoundedRectangle(cornerRadius: 100).stroke(Color.red, lineWidth: 2))
+                            .padding(.vertical)
+                        
+                    }
+                    .sheet(isPresented: $mostrarImagePicker){
+                        ImagePicker(sourceType: .photoLibrary, selectedImage: $imageGeneral )
+                    }
+                
+                
+                
+                
+                Button(){
+                    imagenes = session.getData(correoUsuario: (session.session?.email)!)
+                    
+                    
+                }label:{
+                    Text("Getdata")
+                }
+                
+                /*
+                 ForEach(0...imagenes.count, id: \.self) { index in
+                 // Text()
+                 imagenes[index]
+                 }*/
+                
+                Button(){
+                    session.uploadStorage(image: imageGeneral)
+                }label:{
+                    Text("Guardar")
+                }.disabled(imageGeneral.pngData() == nil)
+                
+                
+            } else {
+                LoginView()
+            }
+        }.onAppear {
+            getUser()
+        }
     }
     
     func getUser () {
